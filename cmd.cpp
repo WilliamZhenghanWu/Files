38da31f71eac32af3063fdd96deeaa37b154b77f
WilliamZhenghanWu <101683154+WilliamZhenghanWu@users.noreply.github.com>
2022-03-16 10:14:42 +0800

    cmd.cpp
    -----------------------------Non Site Replication--------------------------
    
    The simulation program developed by Wu Zhenghan, do not steal the copyright of windows CMD!
    
    Creation date: 2022.2.2 21:28
    
    -----------------------------Non Site Replication--------------------------
    
    
    
    -----------------------------非网站复制--------------------------
    本程序为Windows的cmd仿真，由William开发，版权所有，请勿盗取！ 
    创建日期：2022.2.2    21:28 
    -----------------------------非网站复制--------------------------

---
+++ cmd.cpp
@@ -0,0 +1,50 @@
+/*
+-----------------------------Non Site Replication--------------------------
+
+The simulation program developed by Wu Zhenghan, do not steal the copyright of windows CMD!
+
+Creation date: 2022.2.2 21:28
+
+-----------------------------Non Site Replication--------------------------
+
+
+
+-----------------------------非网站复制--------------------------
+本程序为Windows的cmd仿真，由William开发，版权所有，请勿盗取！ 
+创建日期：2022.2.2    21:28 
+-----------------------------非网站复制-------------------------- 
+*/
+#include<bits/stdc++.h>//包含了system
+using namespace std;
+int main(){
+	string a="C:/Users/dell>"; 
+	cout<<"Microsoft Windows [版本 10.0.22000.469]"<<endl;
+	cout<<"(c) Microsoft Corporation。保留所有权利。"<<endl;
+	cout<<endl;
+	while(1){
+	    cout<<a;
+		string s;
+		getline(cin,s);
+		if(s=="exit"){
+			break;//TODO
+		}
+		else if(s=="C:"||s=="c:"){
+			a="C:>";//TODO
+		}
+		else if(s=="D:"||s=="d:"){
+			a="D:/>";//TODO
+		}	
+		else if(s=="E:"||s=="e:"){
+			a="E:/>";//TODO
+		}
+		else if(s=="F:"||s=="f:"){
+			a="F:/>";//TODO
+		}
+		else if(s=="G:"||s=="g:"){
+			a="G:/>";//TODO
+		}		
+	    system(s.c_str());
+		cout<<endl;
+	}
+	return 0; 
+}
